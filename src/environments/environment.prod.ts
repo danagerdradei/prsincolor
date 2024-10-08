7dc5993baa195d29e14e8a0b79806cc0c8cd9226
German David Franco Herrera <47800277+danagerdradei@users.noreply.github.com>
2024-10-07 21:23:03 -0500

    nm,nm,
    nm,nm,

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
-  production: true,
-  baseUrl: 'http://localhost:44342/',
+  production: false,
+  baseUrl: 'http://www.prsincolores.somee.com/',
   methods: {
-    authentication:"api/V1/Authentication",
+    authentication:"Authorization",
     report:"api/V1/Report",
     service:"api/V1/Service",
     status:"api/V1/Status",
     user:"api/V1/User",
-  }
+  },
+  baseFrontUrl:'https://prsincolores.netlify.app'
 };
