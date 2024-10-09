a43d792b624d9fdad740ff919fd85e786a1102ab
German David Franco Herrera <47800277+danagerdradei@users.noreply.github.com>
2024-10-09 08:48:40 -0500

    d
    d

--- Fronted/src/environments/environment.prod.ts
+++ Fronted/src/environments/environment.prod.ts
@@ -8,6 +8,6 @@ export const environment = {
     status:"api/V1/Status",
     user:"api/V1/User",
   },
-  baseFrontUrl:'https://prsincolor-1.onrender.com/',
+  baseFrontUrl:'https://prsincolores.onrender.com',
   clientId: '532980490624-4u9fnec74f0n8ra78pae9ljefrv110k2.apps.googleusercontent.com'
 };
